b26f35b4478382ea26d1bb7bd9e0b5f560d9f00a
Ethan Edwards <ethanmedwards@gmail.com>
2021-09-03 14:13:55 -0400

    Bugs fixed and text changed

--- gen.js
+++ gen.js
@@ -38,7 +38,7 @@ function findCountry(per){
                 } else{
                     population = population + countries[country]['data'].people.population['total'];
                     if(population>per){
-                        console.log("You live in " + country)
+                        //console.log("You live in " + country)
                         return country;
                     }
                 }
@@ -54,8 +54,8 @@ function findAgeBracket(per, country){
     var age = ageArray[4];
     var whatage = Math.random()*1;
     var runningp = countries[country]['data'].people.age_structure[ageArray[0]]["percent"];
-    console.log(per)
-    console.log(runningp)
+    //console.log(per)
+    //console.log(runningp)
     if(per<runningp){
         return 0;
     }
@@ -101,10 +101,10 @@ function getAge(bracket){
 
 function getSex(bracket){
     var whatsex = Math.random()*1;
-    console.log("get sex")
-    console.log(ageArray[bracket])
-    console.log(countries[country]['data'].people.sex_ratio["by_age"])
-    console.log(countries[country]['data'].people.sex_ratio["by_age"][ageArray[bracket]])
+    //console.log("get sex")
+    //console.log(ageArray[bracket])
+    //console.log(countries[country]['data'].people.sex_ratio["by_age"])
+    //console.log(countries[country]['data'].people.sex_ratio["by_age"][ageArray[bracket]])
     var rat = "";
     if(bracket==4){
         rat = countries[country]['data'].people.sex_ratio["by_age"]["65_years_and_over"]["value"];
@@ -124,8 +124,8 @@ function getSex(bracket){
 
 function getLiterate(perc, sex, age){
     if(age>15){
-        console.log("undefined?")
-        console.log(countries[country]['data'].people);
+        //console.log("undefined?")
+        //console.log(countries[country]['data'].people);
         try{
             rate = countries[country]['data'].people.literacy[sex]["value"];
             if(perc<rate){
@@ -180,10 +180,10 @@ function getUrban(perc){
 function getUrbanArea(countryperson){
     var urblist = countries[country]['data'].people.major_urban_areas["places"]
     for(var i = 0; i < urblist.length; i++){
-        console.log("area");
-        console.log(urblist[i]["place"])
-        console.log(urblist[i]["population"]);
-        console.log(countryperson)
+        //console.log("area");
+        //console.log(urblist[i]["place"])
+        //console.log(urblist[i]["population"]);
+        //console.log(countryperson)
         if (countryperson < urblist[i]["population"]){
             return urblist[i]["place"];
         }
@@ -193,7 +193,7 @@ function getUrbanArea(countryperson){
         
     }
     //if it fails to find one
-    return "non-major urban area"
+    return "a non-major urban area"
 }
 
 function getDrinkingWaterSource(urban, perc){
@@ -201,12 +201,20 @@ function getDrinkingWaterSource(urban, perc){
     if(urban){
         urb = "urban"
     }
-    console.log("drink")
-    console.log(countries[country]['data'].people);
-    drinkp = countries[country]['data'].people.drinking_water_source["improved"][urb]["value"];
+    //console.log("drink")
+    //console.log(countries[country]['data'].people);
+    var drinkp;
+    //console.log(Object.keys(countries[country]['data'].people.drinking_water_source["improved"]));
+    if(Object.keys(countries[country]['data'].people.drinking_water_source["improved"])[0]=="urban"){
+        //console.log("urban success");
+        drinkp = countries[country]['data'].people.drinking_water_source["improved"][urb]["value"];
+    }
+    else{
+        drinkp = countries[country]['data'].people.drinking_water_source["improved"]["total"]["value"];
+    }
     
-    console.log(perc)
-    console.log(drinkp)
+    //console.log(perc)
+    //console.log(drinkp)
     if(perc<drinkp){
         return "improved";
     }
@@ -231,9 +239,9 @@ function getSanitationFacility(urban, perc){
 
 function getEmployment(urban, perc){
     //Use most recent rate
-    console.log("employment");
-    console.log(countries[country]['data'].economy);
-    console.log(countries[country]['data'].economy.unemployment_rate.annual_values[0]);
+    //console.log("employment");
+    //console.log(countries[country]['data'].economy);
+    //console.log(countries[country]['data'].economy.unemployment_rate.annual_values[0]);
     try{
         unp = countries[country]['data'].economy.unemployment_rate.annual_values[0]["value"];
         if(perc<unp){
@@ -245,9 +253,9 @@ function getEmployment(urban, perc){
     }
     
     if(!urban){
-        console.log("rural job");
+        //console.log("rural job");
         var ruralp = 100-countries[country]['data'].people.urbanization.urban_population["value"];
-        console.log(countries[country]['data'].economy.labor_force["by_occupation"]);
+        //console.log(countries[country]['data'].economy.labor_force["by_occupation"]);
         var farmp = countries[country]['data'].economy.labor_force["by_occupation"].occupation["agriculture"]["value"];
         if(perc*ruralp/100.0<farmp){
             return "an agricultural worker";
@@ -263,15 +271,15 @@ function getElectric(urban, perc){
     if(urban){
         ur = "urban_electrification"
     }
-    console.log("electric")
-    console.log(countries[country]['data'].energy);
+    //console.log("electric")
+    //console.log(countries[country]['data'].energy);
     if(electricp = JSON.stringify(countries[country]['data'].energy.electricity.access).includes(ur)){
         electricp = countries[country]['data'].energy.electricity.access[ur]["value"]
-        console.log("proper value");
+        //console.log("proper value");
     }
     else{
         electricp = countries[country]['data'].energy.electricity.access["total_electrification"]["value"];
-        console.log("improper value");
+        //console.log("improper value");
     }
     if(perc<electricp){
         return "access to electricity";
@@ -288,7 +296,7 @@ function countCountry(){
         if(count>0){
             try{
                 if(country=='european_union'){
-                    console.log(countries[country]['data'].people.population['total'])
+                    //console.log(countries[country]['data'].people.population['total'])
                 } else{
                     pop = pop + countries[country]['data'].people.population['total'];
                 }
@@ -299,6 +307,26 @@ function countCountry(){
         count++;
     }
     return pop;
+}
+
+function capitalizeWord(word){
+    var rword = word;
+    //console.log("ecuador");
+    if(rword===undefined){
+        //console.log("hi");
+        rword = "undefined";
+    }
+    rword = rword[0].toUpperCase()+rword.substring(1)
+    while(rword.includes("_")){
+        sub = rword.indexOf("_");
+        rword = rword.substring(0, sub) + " " + rword[sub+1].toUpperCase() + rword.substring(sub+2);
+    }
+    //eliminate sentences
+    if(rword.length>30){
+        rword = rword.substring(0, rword.indexOf(" "));
+    }
+
+    return rword;
 }
 
 
@@ -332,7 +360,7 @@ function generatePerson(){
     var urbanperc = Math.random() * 100;
     var urban = getUrban(urbanperc);
 
-    var urbanarea = "rural area";
+    var urbanarea = "a rural area";
     if(urban){  
         countryperson = (urbanperc/100) * countrypop;
         urbanarea = getUrbanArea(countryperson);
@@ -350,6 +378,7 @@ function generatePerson(){
     var elecp = Math.random() * 100;
     var elec = getElectric(urban, elecp);
 
+    /*
     console.log(population);
     console.log("You are person number " + person)
     console.log("You are a " + age + " year old " + sex + " from " + country + " and are " + literate);
@@ -360,8 +389,41 @@ function generatePerson(){
     console.log(sanitation)
     console.log(job);
     console.log(elec)
+    */
 
-    var result = "You are a " + age + " year old " + sex + " living in " + country + "." + "You are " + eth + ", and of " + rel + " religion. " + "You live in " + urbanarea + ", with " + elec + " and access to " + drinkingwater + " drinking water and " + sanitation + " sanitation."
+    //transform text
+    if(rel=="none"){
+        rel = "no"
+    }
+    var countryword = capitalizeWord(country);
+    if(countryword=="Burma"){
+        countryword="Taiwan";
+    }
+    
+    var ethword = capitalizeWord(eth);
+    if(ethword=="Other"){
+        ethword = "a minority ethnicity"
+    }
+
+    var relword = capitalizeWord(rel);
+    if(relword.includes("religion")){
+        relword = relword.substring(0, relword.indexOf("religion")-1)
+        //console.log("Did religion shorten?")
+    }
+    if(relword=="none"){
+        relword = "no"
+    }
+
+    var agearticle = "a";
+    if(age==8||age==11||age==18||(age>79&&age<90)){
+        agearticle = "an";
+    }
+
+    var result = "You are " + agearticle + " " + age + " year old " + sex + " living in " + countryword + "." + " You are " + ethword + " and of " + relword + " religion. " + "You live in " + urbanarea + ", with " + elec + " and access to " + drinkingwater + " drinking water and " + sanitation + " sanitation."
+
+    if(eth=="none"||eth=="undefined"){
+        result = "You are a " + age + " year old " + sex + " living in " + countryword + "." + " You are of " + relword + " religion. " + "You live in " + urbanarea + ", with " + elec + " and access to " + drinkingwater + " drinking water and " + sanitation + " sanitation."
+    }
     
     if(age>18){
         result = result + " You are " + job + ".";  
@@ -371,7 +433,7 @@ function generatePerson(){
 }
 
 function timer(tim){
-    console.log("begin timer");
+    //console.log("begin timer");
     var timeleft = tim;
     var downloadTimer = setInterval(function(){
     if(timeleft <= 0){
